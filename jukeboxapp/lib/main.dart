cf7fd83babc376e77e53fee364fa4da02bfed9d6
KryzAlePC <kryzale@gmail.com>
2021-03-01 13:23:41 -0500

    Implementado Home

--- jukeboxapp/lib/main.dart
+++ jukeboxapp/lib/main.dart
@@ -1,60 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_unity_widget/flutter_unity_widget.dart';
+import 'package:get/get.dart';
+import 'package:jukeboxapp/screens/onboard/onboard.dart';
 
 void main() {
-  runApp(MaterialApp(home: UnityDemoScreen()));
+  runApp(MyApp());
 }
 
-class UnityDemoScreen extends StatefulWidget {
-  UnityDemoScreen({Key key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
-  _UnityDemoScreenState createState() => _UnityDemoScreenState();
-}
-
-class _UnityDemoScreenState extends State<UnityDemoScreen> {
-  static final GlobalKey<ScaffoldState> _scaffoldKey =
-      GlobalKey<ScaffoldState>();
-  UnityWidgetController _unityWidgetController;
-
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      body: SafeArea(
-        bottom: false,
-        child: WillPopScope(
-          onWillPop: () {
-            // Pop the category page if Android back button is pressed.
-          },
-          child: Stack(
-            children: <Widget>[
-              UnityWidget(
-                onUnityCreated: onUnityCreated,
-                isARScene: true,
-              ),
-              Positioned(
-                child: ElevatedButton(
-                  onPressed: () {
-                    cambiarCancion(
-                        "https://dl20.freemp3downloads.online/file/youtubeNWMRx4G9zjo128.mp3?fn=EL%20CUARTETO%20DE%20IBAI%20(Versi%C3%B3n%20Oficial)%20ft.%20Lucas%20Requena%20%26%20ortoPilot.mp3");
-                  },
-                  child: Text('Iniciar'),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return GetMaterialApp(
+      home: Onboard(),
+      theme: ThemeData(primaryColor: const Color(0xFF4A00E0)),
     );
-  }
-
-  // Callback that connects the created controller to the unity controller
-  void onUnityCreated(controller) {
-    this._unityWidgetController = controller;
-  }
-
-  void cambiarCancion(String url) {
-    _unityWidgetController.postMessage('AudioManager', 'cambiarUrl', url);
-    print(url);
   }
 }
